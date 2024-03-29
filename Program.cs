480031aaef31156f17c2925312d50418dae5c93d
Kozell Yon-Brooks <ko3lyonbrooks@gmail.com>
2024-03-29 13:07:52 +0000

    fix: Move UseResponseCompression() before UseHttpsRedirection()
    - Corrected the order of middleware in Program.cs to ensure response compression is applied before HTTPS redirection.

--- Program.cs
+++ Program.cs
@@ -40,9 +40,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
+    app.UseResponseCompression();
 }
 
-app.UseResponseCompression();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
